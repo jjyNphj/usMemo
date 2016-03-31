dffbe1594a5bf8b88be7c9bf0625af575700fa3b
kiela929 <kiela929@naver.com>
2016-03-31 23:21:22 +0900

    초기설정

---
+++ UsMemo/src/main/java/com/twogather/usMemo/dto/BoardInfo.java
@@ -0,0 +1,24 @@
+package com.twogather.usMemo.dto;
+
+public class BoardInfo {
+	
+	private int bNum;
+	private String name;
+	
+	public int getbNum() {
+		return bNum;
+	}
+	public void setbNum(int bNum) {
+		this.bNum = bNum;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
+}
---
+++ UsMemo/src/main/java/com/twogather/usMemo/dto/Member.java
@@ -0,0 +1,36 @@
+package com.twogather.usMemo.dto;
+
+public class Member {
+	private int num;
+	private String id;
+	private String name;
+	private String picture;
+	
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	
+
+}
